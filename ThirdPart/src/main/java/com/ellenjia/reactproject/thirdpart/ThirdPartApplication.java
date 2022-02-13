7d0deb9a344077c553bef98845771a0888ce4952
ellenjia <971472965@qq.com>
2022-02-14 01:33:12 +0800

    Initial commit

---
+++ ThirdPart/src/main/java/com/ellenjia/reactproject/thirdpart/ThirdPartApplication.java
@@ -0,0 +1,13 @@
+package com.ellenjia.reactproject.thirdpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdPartApplication.class, args);
+    }
+
+}
---
+++ ThirdPart/src/test/java/com/ellenjia/reactproject/thirdpart/ThirdPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ellenjia.reactproject.thirdpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
